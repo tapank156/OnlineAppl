7539ef6fd8929e42231938499db295c0f8fd14d5
tapank156 <tapank156@gmail.com>
2014-02-04 18:39:33 +0530

    Add changes

--- JAXBStudy/src/com/tkd/jaxb/Family.java
+++ JAXBStudy/src/com/tkd/jaxb/Family.java
@@ -8,6 +8,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
+//Add comments
 @XmlRootElement(name = "family")
 public class Family {	
 	private List<Person> member;
