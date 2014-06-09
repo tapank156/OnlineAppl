7dd840c417ef2f9a4a141b15d81f6fd462d2a08f
tapank156 <tapank156@gmail.com>
2014-06-09 12:07:48 +0530

    Adding initial version for this calls

---
+++ JAXBStudy/src/com/tkd/jaxb/ANewClass.java
@@ -0,0 +1,23 @@
+package com.tkd.jaxb;
+
+public class ANewClass {
+	private String name;
+	private int salary;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public ANewClass(String name, int salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}	
+}
