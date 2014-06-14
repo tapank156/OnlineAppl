79b47d108e86fc485cbe3feac9fb1b0e6df0cb1e
tapan <tapan@tkdinda>
2014-06-14 19:37:39 +0530

    Added again

--- JAXBStudy/src/com/tkd/jaxb/ANewClass.java
+++ JAXBStudy/src/com/tkd/jaxb/ANewClass.java
@@ -1,29 +1,30 @@
+
 package com.tkd.jaxb;
 
 /*Just a comment */
 
 public class ANewClass {
-	private String name;
-	private int salary;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getSalary() {
-		return salary;
-	}
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-	public ANewClass(String name, int salary) {
-		super();
-		this.name = name;
-		this.salary = salary;
-	}
-	@Override
-	public String toString() {
-		return "ANewClass [name=" + name + ", salary=" + salary + "]";
-	}	
+private String name;
+private int salary;
+public String getName() {
+return name;
+}
+public void setName(String name) {
+this.name = name;
+}
+public int getSalary() {
+return salary;
+}
+public void setSalary(int salary) {
+this.salary = salary;
+}
+public ANewClass(String name, int salary) {
+super();
+this.name = name;
+this.salary = salary;
+}
+@Override
+public String toString() {
+return "ANewClass [name=" + name + ", salary=" + salary + "]";
+}	
 }
